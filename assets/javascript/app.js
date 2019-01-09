7635b62d9296aadbe9f48c824272606b348e1b30
Jonathan Mai <h.jmai2791@gmail.com>
2019-01-09 02:03:49 -0800

    change relative path

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,9 @@
-let currentPicture = ""
+let currentPicture = "1"
 $("#my-carousel").on('slide.bs.carousel', function(e) {
     $('#hero-banner2').animate({opacity: 0}, 0).css("background-image", `url(./assets/images/bgs/hero${e.to+1}.jpg)`).animate({opacity: 1}, 800);
     currentPicture = e.to+1;
 })
 
 $('.expand').on('click', function() {
-    window.open('/assets/images/pics/camera'+currentPicture+'.jpg');
+    window.open('./assets/images/pics/camera'+currentPicture+'.jpg');
 })
