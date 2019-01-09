fd0c119b82eaa013128b9597cb43f8050a74a58d
Jonathan Mai <h.jmai2791@gmail.com>
2019-01-09 02:00:37 -0800

    open image added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,9 @@
+let currentPicture = ""
 $("#my-carousel").on('slide.bs.carousel', function(e) {
-    $("#hero-banner").removeClass(`hero${e.from+1}`);
-    $("#hero-banner").addClass(`hero${e.to+1}`);
-    console.log(`from: ${e.from+1} to: ${e.to+1}`)
+    $('#hero-banner2').animate({opacity: 0}, 0).css("background-image", `url(./assets/images/bgs/hero${e.to+1}.jpg)`).animate({opacity: 1}, 800);
+    currentPicture = e.to+1;
+})
+
+$('.expand').on('click', function() {
+    window.open('/assets/images/pics/camera'+currentPicture+'.jpg');
 })
