99fffd419d7be7b4ff98ac24ec6abd603d268e38
Jonathan Mai <h.jmai2791@gmail.com>
2019-01-14 01:04:32 -0800

    form submission added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+let address = ""
 let currentPicture = "1"
 $("#my-carousel").on('slide.bs.carousel', function(e) {
     $('#hero-banner2').animate({opacity: 0}, 0).css("background-image", `url(./assets/images/bgs/hero${e.to+1}.jpg)`).animate({opacity: 1}, 800);
@@ -19,22 +20,75 @@ modal.onclick = function() {
     modal.style.display = "none";
 }
 
+// Google maps autocomplete
+var placeSearch, autocomplete;
+var componentForm = {
+  street_number: 'short_name',
+  route: 'long_name',
+  locality: 'long_name',
+  administrative_area_level_1: 'short_name',
+  country: 'long_name',
+  postal_code: 'short_name'
+};
 
-var $form = $('#form-submission'),
-        url = 'https://script.google.com/macros/s/AKfycbxJ0aFoZv0MhXNAncPRyUFcVfR0Y41lUJqtGeZ3QXdvBcnUZZA/exec';
+function initAutocomplete() {
+  // Create the autocomplete object, restricting the search to geographical
+  // location types.
+  autocomplete = new google.maps.places.Autocomplete(
+      /** @type {!HTMLInputElement} */(document.getElementById('home_address')),
+      {types: ['geocode']});
+
+  // When the user selects an address from the dropdown, populate the address
+  // fields in the form.
+  autocomplete.addListener('place_changed', fillInAddress);
+}
+
+function fillInAddress() {
+  // Get the place details from the autocomplete object.
+  var place = autocomplete.getPlace();
+  address = place.formatted_address.split(", USA")[0];
+  console.log(address);
+  $('#home_address').val(address);
+}
+
+let modalSuccess = document.getElementById("success");
+const successSpan = document.getElementsByClassName('closeSuccess')[0];
+successSpan.onclick = function() {
+    console.log('hi');
+    modalSuccess.style.display = "none";
+}
+modalSuccess.onclick = function() {
+    modalSuccess.style.display = "none";
+}
 $("#form-submission").submit(function(e) {
+    var form = $('#form-submission'),
+        url = ``;
+
     e.preventDefault();
+    url = `https://script.google.com/macros/s/AKfycbxJ0aFoZv0MhXNAncPRyUFcVfR0Y41lUJqtGeZ3QXdvBcnUZZA/exec`;
     var jqxhr = $.ajax({
         url: url,
         method: "GET",
         dataType: "json",
-        data: $form.serializeObject()
-    }).success(
-        // do something
-    )
-    // console.log('hi');
-    // var $form = $('#form-submission');
-    // console.log($form.serializeObject());
+        data: form.serializeObject()
+    }).then(function(res){
+        if(res.result == 'success'){
+            $('#name').val('');
+            $('#email_address').val('');
+            $('#home_address').val('');
+            $('#guests').val('');
+            modalSuccess.style.display = "block";
+        }
+    });
 })
 
-// https://script.google.com/macros/s/AKfycbxJ0aFoZv0MhXNAncPRyUFcVfR0Y41lUJqtGeZ3QXdvBcnUZZA/exec
+$(document).ready(function(){
+    $(window).keydown(function(event){
+        if(event.keyCode == 13) {
+            event.preventDefault();
+            return false;
+        }
+    })
+})
+
+// https://hjmai.github.io/lanviandjon2019
