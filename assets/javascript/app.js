b212513f06502976a2f510450b05737366397cb7
Jonathan Mai <h.jmai2791@gmail.com>
2019-01-13 20:05:52 -0800

    image size changes and started contact form

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,7 +8,7 @@ let modal = document.getElementById("myModal");
 let modalImg = document.getElementById('img01');
 $('.expand').on('click', function() {
     modal.style.display = "block";
-    modalImg.src = `./assets/images/pics/camera${currentPicture}.jpg`;
+    modalImg.src = `./assets/images/pics/full/camera${currentPicture}.jpg`;
 });
 
 const span = document.getElementsByClassName('close')[0];
@@ -18,3 +18,23 @@ span.onclick = function() {
 modal.onclick = function() {
     modal.style.display = "none";
 }
+
+
+var $form = $('#form-submission'),
+        url = 'https://script.google.com/macros/s/AKfycbxJ0aFoZv0MhXNAncPRyUFcVfR0Y41lUJqtGeZ3QXdvBcnUZZA/exec';
+$("#form-submission").submit(function(e) {
+    e.preventDefault();
+    var jqxhr = $.ajax({
+        url: url,
+        method: "GET",
+        dataType: "json",
+        data: $form.serializeObject()
+    }).success(
+        // do something
+    )
+    // console.log('hi');
+    // var $form = $('#form-submission');
+    // console.log($form.serializeObject());
+})
+
+// https://script.google.com/macros/s/AKfycbxJ0aFoZv0MhXNAncPRyUFcVfR0Y41lUJqtGeZ3QXdvBcnUZZA/exec
