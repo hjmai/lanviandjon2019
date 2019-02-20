b8a28b9c3daf25bf9766fa977c95166a27cb3759
Jonathan Mai <h.jmai2791@gmail.com>
2019-02-19 19:46:36 -0800

    success message

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -80,18 +80,18 @@ $(".rsvp-form").submit(function(e) {
         data: form.serializeObject()
     }).then(function(res){
         if(res.result == 'success') {
-            var successCheckmark = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 130.2 130.2">
+            var successCheckmark = `<div class="success-checkmark">Successful!<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 130.2 130.2">
                             <circle class="path circle" fill="none" stroke="#73AF55" stroke-width="6" stroke-miterlimit="10" cx="65.1" cy="65.1" r="62.1"/>
                             <polyline class="path check" fill="none" stroke="#73AF55" stroke-width="6" stroke-linecap="round" stroke-miterlimit="10" points="100.2,40.2 51.5,88.8 29.8,67.5 "/>
-                        </svg>`;
+                        </svg></div>`;
             $(".loader-container").html(successCheckmark)
             $('.name').val('');
             $('#email_address').val('');
             $('#home_address').val('');
             $('#guests').val('');
-            setTimeout(() => {
-                modalSuccess.style.display = "none";
-            }, 1500);
+            // setTimeout(() => {
+            //     modalSuccess.style.display = "none";
+            // }, 1500);
         }
     });
 });
